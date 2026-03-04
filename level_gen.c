2aa49660ef787ba42a0bb1b13e489e54fe1903b7
Nimrod Libman <replace@this.com>
2026-03-04 20:53:12 +0000

    Break engine up so that level generation can be its own module

--- level_gen.c
+++ level_gen.c
@@ -2,25 +2,6 @@
 #include "log.c"
 #include <stdbool.h>
 
-unsigned add_entity(Level* level, Entity entity)
-{
-    unsigned ec = level->entity_count++;
-    if (ec >= MAX_ENTITIES) {
-        // UH OH!!
-        printf("We hit the entity limit!\n");
-    }
-    level->entities[ec] = entity;
-    return ec;
-}
-
-unsigned add_wall(Level* level, int x, int y)
-{
-    Entity wall = { 0 };
-    wall.type = WALL;
-    wall.position = (Vector2Int) { x, y };
-    return add_entity(level, wall);
-}
-
 typedef struct {
     int top;
     int bottom;
@@ -210,7 +191,7 @@ void bsp_dungeon(Level* level, int width, int height)
     for (int i = 0; i < width * height; i++) {
         tilemap[i] = 1;
     }
-    bsp_iter(level, 3, (rect_t) { .bottom = 0, .top = height - 1, .left = 0, .right = width - 1 }, tilemap, width, height);
+    bsp_iter(level, 3, (rect_t){ .bottom = 0, .top = height - 1, .left = 0, .right = width - 1 }, tilemap, width, height);
 
     // For every filled cell in the output tilemap, place a wall.
     for (int x = 0; x < width; x++) {
@@ -225,5 +206,23 @@ void bsp_dungeon(Level* level, int width, int height)
 
 void generate_level(Level* level)
 {
-    bsp_dungeon(level, LEVEL_WIDTH, LEVEL_HEIGHT);
+	bsp_dungeon(level, LEVEL_WIDTH, LEVEL_HEIGHT);
+
+	// Level Generation
+	for (int x = 1; x < LEVEL_WIDTH - 1; x++) {
+		Wall(&level.entities[level.entity_count++], x, 0);
+		Wall(&level.entities[level.entity_count++], x, LEVEL_HEIGHT - 1);
+	}
+	for (int y = 1; y < LEVEL_HEIGHT - 1; y++) {
+		Wall(&level.entities[level.entity_count++], 0, y);
+		Wall(&level.entities[level.entity_count++], LEVEL_WIDTH - 1, y);
+	}
+
+	Goblin(&level.entities[level.entity_count++], 2, 2);
+
+	// Spawn some items
+	Health(&level.entities[level.entity_count++], 5, 5, 10);
+	Gold(&level.entities[level.entity_count++], 8, 10, 10);
+	Armor(&level.entities[level.entity_count++], 15, 15, 10);
+	Arrows(&level.entities[level.entity_count++], 20, 20, 10);
 }
