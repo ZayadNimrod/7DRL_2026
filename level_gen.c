a324d2c8b4b9eacc1b7fe5fe2a6dd0063e577399
Nimrod Libman <replace@this.com>
2026-03-05 19:46:00 +0000

    DRAFT BSP generation

--- level_gen.c
+++ level_gen.c
@@ -1,42 +1,128 @@
 #include "ecs.c"
+#include <stdlib.h>
 
 unsigned add_entity(Level* level, Entity entity)
 {
-	unsigned ec = level->entity_count++;
-	if (ec >= MAX_ENTITIES) {
-		// UH OH!!
-		printf("We hit the entity limit!\n");
-	}
-	level->entities[ec] = entity;
-	return ec;
+    unsigned ec = level->entity_count++;
+    if (ec >= MAX_ENTITIES) {
+        // UH OH!!
+        printf("We hit the entity limit!\n");
+    }
+    level->entities[ec] = entity;
+    return ec;
 }
 
 unsigned add_wall(Level* level, int x, int y)
 {
-	Entity wall = { 0 };
-	wall.type = WALL;
-	wall.position = (Vector2Int) { x, y };
-	return add_entity(level, wall);
+    Entity wall = { 0 };
+    wall.type = WALL;
+    wall.position = (Vector2Int) { x, y };
+    return add_entity(level, wall);
+}
+
+typedef struct {
+    int top;
+    int bottom;
+    int left;
+    int right;
+} rect_t;
+
+void bsp_iter(Level* level, unsigned remaining_depth, rect_t* bounding_box, char* tilemap, unsigned width, unsigned height)
+{
+    if (remaining_depth == 0 || bounding_box->top - bounding_box->bottom < 4 || bounding_box->right - bounding_box->left < 4) {
+        // TODO perturb these
+        unsigned x_start = bounding_box->left + 1;
+        unsigned x_end = bounding_box->right - 1;
+        unsigned y_start = bounding_box->bottom + 1;
+        unsigned y_end = bounding_box->top - 1;
+
+        // Carve out the room
+        for (unsigned x = x_start; x <= x_end; x++) {
+            for (unsigned y = y_start; y <= y_end; y++) {
+                tilemap[y * width + x] = 0;
+            }
+        }
+
+        // Place monsters in it
+
+        // TODO
+
+    } else {
+        // Bisect the bounding box
+        rect_t bb1 = *bounding_box;
+        rect_t bb2 = *bounding_box;
+
+        unsigned split_direction = rand() % 2;
+        if (split_direction) {
+            int top = bounding_box->top;
+            int bottom = bounding_box->bottom;
+            unsigned halfrange = (top - bottom) / 2;
+            unsigned split_point = rand() % halfrange + halfrange / 2 + bottom;
+            bb1.top = top;
+            bb1.bottom = split_point;
+            bb2.top = split_point;
+            bb2.bottom = bottom;
+
+        } else {
+            int right = bounding_box->right;
+            int left = bounding_box->left;
+            unsigned halfrange = (right - left) / 2;
+            unsigned split_point = rand() % halfrange + halfrange / 2 + left;
+            bb1.left = left;
+            bb1.right = split_point;
+            bb2.left = split_point;
+            bb2.right = right;
+        }
+
+        // Build the sub-partitions
+        bsp_iter(level, remaining_depth - 1, &bb1, tilemap, width, height);
+        bsp_iter(level, remaining_depth - 1, &bb2, tilemap, width, height);
+
+        // Join the sub-partitions
+        Vector2Int p1, p2;
+
+        // TODO
+    }
+}
+
+void bsp_dungeon(Level* level)
+{
+
+    int width = LEVEL_WIDTH;
+    int height = LEVEL_HEIGHT;
+    // Call bsp_iter on the top level
+    char tilemap[width * height];
+    for (int i = 0; i < width * height; i++) {
+        tilemap[i] = 1;
+    }
+    bsp_iter(level, 4, &(rect_t) { 0, height - 1, 0, width - 1 }, tilemap, width, height);
+
+    // For every filled cell in the output tilemap, place a wall.
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            if (tilemap[y * width + x]) {
+                Wall(&level->entities[level->entity_count++], x, y);
+            }
+        }
+    }
 }
 
 void generate_level(Level* level)
 {
 
-	// Level Generation
-	for (int x = 1; x < LEVEL_WIDTH - 1; x++) {
-		Wall(&level.entities[level.entity_count++], x, 0);
-		Wall(&level.entities[level.entity_count++], x, LEVEL_HEIGHT - 1);
-	}
-	for (int y = 1; y < LEVEL_HEIGHT - 1; y++) {
-		Wall(&level.entities[level.entity_count++], 0, y);
-		Wall(&level.entities[level.entity_count++], LEVEL_WIDTH - 1, y);
-	}
+    // Level Generation
+    for (int x = 1; x < LEVEL_WIDTH - 1; x++) {
+        add_wall(level, x, 0);
+        add_wall(level, x, LEVEL_HEIGHT - 1);
+    }
+    for (int y = 1; y < LEVEL_HEIGHT - 1; y++) {
+        add_wall(level, 0, y);
+        add_wall(level, LEVEL_WIDTH - 1, y);
+    }
 
-	Goblin(&level.entities[level.entity_count++], 2, 2);
-
-	// Spawn some items
-	Health(&level.entities[level.entity_count++], 5, 5, 10);
-	Gold(&level.entities[level.entity_count++], 8, 10, 10);
-	Armor(&level.entities[level.entity_count++], 15, 15, 10);
-	Arrows(&level.entities[level.entity_count++], 20, 20, 10);
+    Entity* goblin = &(level->entities[level->entity_count++]);
+    goblin->type = ENEMY;
+    goblin->position = (Vector2Int) { 2, 2 };
+    goblin->hp = 20;
+    goblin->damage = 2;
 }
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "engine.c"
 #include "log.c"
 #include <ncurses.h>
+#include <stdlib.h>
 
 typedef struct {
 	char character;
@@ -18,7 +19,7 @@ display_t entity_char(Entity* entity)
 	case WALL:
 		return (display_t) { .character = '#', .attributes = A_NORMAL };
 	case ENEMY:
-		return (display_t) { .character = 'e', .attributes = COLOR_PAIR(1)};        
+		return (display_t) { .character = 'e', .attributes = COLOR_PAIR(1) };
 	default:
 		return (display_t) { .character = '?', .attributes = A_BLINK };
 	}
@@ -28,8 +29,8 @@ WINDOW* map_window;
 WINDOW* log_window;
 WINDOW* stat_window;
 
-
-void render_stats(Level* world){
+void render_stats(Level* world)
+{
 	(void)world;
 }
 
@@ -40,7 +41,7 @@ void render_map(Level* world)
 	for (unsigned i = 0; i < world->entity_count; i++) {
 		Entity* e = &world->entities[i];
 		display_t d = entity_char(e);
-		if (d.character!=0){
+		if (d.character != 0) {
 			wattron(map_window, d.attributes);
 			Vector2Int position = e->position;
 			mvwaddch(map_window, position.y, position.x, d.character);
@@ -54,14 +55,14 @@ void render_map(Level* world)
 void render_log(logger_t* logger)
 {
 	wclear(log_window);
-	if (logger->total_logs>=logger->max_logs){
+	if (logger->total_logs >= logger->max_logs) {
 		for (unsigned i = 0; i < logger->max_logs; i++) {
 			wmove(log_window, i, 0);
 			unsigned idx = (logger->idx + i) % logger->max_logs;
 			char* log = logger->logs[idx];
 			waddstr(log_window, log);
 		}
-	} else{
+	} else {
 		for (unsigned i = 0; i < logger->total_logs; i++) {
 			wmove(log_window, i, 0);
 			char* log = logger->logs[i];
@@ -88,20 +89,23 @@ int quit()
 int main()
 {
 	const int MAX_LOGS = LEVEL_HEIGHT / 2;
-	const int MAX_LOG_LEN = 120 - LEVEL_WIDTH;
+	const int MAX_LOG_LEN = 80 - LEVEL_WIDTH;
 	logger_t logger = *init_logger(MAX_LOGS, MAX_LOG_LEN);
+
+	//srand(time(0));
+	srand(0);
+
 
 	initscr();
 	start_color();
 	init_pair(0, COLOR_WHITE, COLOR_BLACK);
 	init_pair(1, COLOR_RED, COLOR_BLACK);
-	//init_pair(2, COLOR_ORANGE, COLOR_BLACK);
+	// init_pair(2, COLOR_ORANGE, COLOR_BLACK);
 	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(3, COLOR_GREEN, COLOR_BLACK);
 	init_pair(4, COLOR_CYAN, COLOR_BLACK);
 	init_pair(5, COLOR_BLUE, COLOR_BLACK);
 	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
-	
 
 	curs_set(0); // hide cursor
 	noecho();
@@ -113,8 +117,7 @@ int main()
 
 	map_window = newwin(LEVEL_HEIGHT, LEVEL_WIDTH, 0, 0);
 	log_window = newwin(MAX_LOGS, MAX_LOG_LEN, LEVEL_HEIGHT - MAX_LOGS, LEVEL_WIDTH);
-	stat_window = newwin(LEVEL_HEIGHT-MAX_LOGS-1, MAX_LOG_LEN, 0, LEVEL_WIDTH);
-
+	stat_window = newwin(LEVEL_HEIGHT - MAX_LOGS - 1, MAX_LOG_LEN, 0, LEVEL_WIDTH);
 
 	log_msg(&logger, "This is a log");
 	log_msg(&logger, "This is another log");
@@ -122,7 +125,7 @@ int main()
 
 	InputAction last_action;
 	while (true) {
-		render(&level,&logger);
+		render(&level, &logger);
 
 		char input = getch();
 
