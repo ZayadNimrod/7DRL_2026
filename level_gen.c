de0e8746e69a71af8e46dd828bb0c1b7e1655789
Robert McCann <robert.mccann@codethink.co.uk>
2026-03-06 17:14:23 +0000

    Fix level_gen.c

--- level_gen.c
+++ level_gen.c
@@ -210,19 +210,19 @@ void generate_level(Level* level)
 
 	// Level Generation
 	for (int x = 1; x < LEVEL_WIDTH - 1; x++) {
-		Wall(&level.entities[level.entity_count++], x, 0);
-		Wall(&level.entities[level.entity_count++], x, LEVEL_HEIGHT - 1);
+		Wall(&level->entities[level->entity_count++], x, 0);
+		Wall(&level->entities[level->entity_count++], x, LEVEL_HEIGHT - 1);
 	}
 	for (int y = 1; y < LEVEL_HEIGHT - 1; y++) {
-		Wall(&level.entities[level.entity_count++], 0, y);
-		Wall(&level.entities[level.entity_count++], LEVEL_WIDTH - 1, y);
+		Wall(&level->entities[level->entity_count++], 0, y);
+		Wall(&level->entities[level->entity_count++], LEVEL_WIDTH - 1, y);
 	}
 
-	Goblin(&level.entities[level.entity_count++], 2, 2);
+	Goblin(&level->entities[level->entity_count++], 2, 2);
 
 	// Spawn some items
-	Health(&level.entities[level.entity_count++], 5, 5, 10);
-	Gold(&level.entities[level.entity_count++], 8, 10, 10);
-	Armor(&level.entities[level.entity_count++], 15, 15, 10);
-	Arrows(&level.entities[level.entity_count++], 20, 20, 10);
+	Health(&level->entities[level->entity_count++], 5, 5, 10);
+	Gold(&level->entities[level->entity_count++], 8, 10, 10);
+	Armor(&level->entities[level->entity_count++], 15, 15, 10);
+	Arrows(&level->entities[level->entity_count++], 20, 20, 10);
 }
